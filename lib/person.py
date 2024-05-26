ac924c749829e4e3e61b874825ddf719fb3485ba
Favor Vincent <vfavorm77@gmail.com>
2024-05-26 15:27:44 +0300

    instance-methods lab complete

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,17 @@ class Person:
     # Class body goes here
 
     #Instance method definition
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
     pass
+
+person1 = Person()
+person1.talk()
+person1.walk()
+
+person2 = Person()
+person2.talk()
+person2.walk()
